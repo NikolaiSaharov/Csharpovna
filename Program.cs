cfe7a973aecd9fbb70ed4770a4c4bdfbab53070e
NikolaiSaharov <145434551+NikolaiSaharov@users.noreply.github.com>
2023-10-08 13:05:00 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 
 class Program
@@ -13,7 +13,40 @@ class Program
         Console.WriteLine("Для игры нажимайте на кнопки ниже:");
         Console.WriteLine("A-S-D-F-G-H-J-K-L-Z-X-C");
         Console.WriteLine("Для переключения меджу октавами нажимайте F5, F6");
-        Console.WriteLine("");
+        
+
+        static int GetNoteIndex(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.A:
+                    return 0;
+                case ConsoleKey.S:
+                    return 1;
+                case ConsoleKey.D:
+                    return 2;
+                case ConsoleKey.F:
+                    return 3;
+                case ConsoleKey.G:
+                    return 4;
+                case ConsoleKey.H:
+                    return 5;
+                case ConsoleKey.J:
+                    return 6;
+                case ConsoleKey.K:
+                    return 7;
+                case ConsoleKey.L:
+                    return 8;
+                case ConsoleKey.Z:
+                    return 9;
+                case ConsoleKey.X:
+                    return 10;
+                case ConsoleKey.C:
+                    return 11;
+                default:
+                    return -1;
+            }
+        }
 
         while (true)
         {
@@ -21,85 +54,49 @@ class Program
             {
                 ConsoleKeyInfo key = Console.ReadKey(true);
 
-                
-                if (key.Key == ConsoleKey.F4)
+            
+            if (key.Key == ConsoleKey.F5)
+            {
+                currentOctave = 2;
+                Console.WriteLine("2 октава");
+            }
+            else if (key.Key == ConsoleKey.F6)
+            {
+                currentOctave = 3;
+                Console.WriteLine("3 октава");
+            }
+            else
+            {
+                int noteIndex = GetNoteIndex(key.Key);
+                if (noteIndex != -1)
                 {
-                    currentOctave = 1;
-                    Console.WriteLine("1 октава");
-                }
-                else if (key.Key == ConsoleKey.F5)
-                {
-                    currentOctave = 2;
-                    Console.WriteLine("2 октава");
-                }
-                else if (key.Key == ConsoleKey.F6)
-                {
-                    currentOctave = 3;
-                    Console.WriteLine("3 октава");
-                }
-                else
-                {
-                    int noteIndex = GetNoteIndex(key.Key);
-                    if (noteIndex != -1)
-                    {
-                        int frequency = GetFrequency(noteIndex);
-                        Console.Beep(frequency, 300);
-                    }
+                    int frequency = GetFrequency(noteIndex);
+                    Console.Beep(frequency, 300);
                 }
             }
         }
     }
-    static int GetFrequency(int noteIndex)
+}
+static int GetFrequency(int noteIndex)
+{
+    int[] octave = GetOctave(currentOctave);
+    return octave[noteIndex];
+}
+static int[] GetOctave(int octaveNumber)
+{
+    switch (octaveNumber)
     {
-        int[] octave = GetOctave(currentOctave);
-        return octave[noteIndex];
-    }
-    static int[] GetOctave(int octaveNumber)
-    {
-        switch (octaveNumber)
-        {
-            
-            case 1:
-                return secondOctave;
-            case 2:
-                return threeOctave;
-            default:
-                return secondOctave;
-        }
-    }
 
-    static int GetNoteIndex(ConsoleKey key)
-    {
-        switch (key)
-        {
-            case ConsoleKey.A:
-                return 0;
-            case ConsoleKey.S:
-                return 1;
-            case ConsoleKey.D:
-                return 2;
-            case ConsoleKey.F:
-                return 3;
-            case ConsoleKey.G:
-                return 4;
-            case ConsoleKey.H:
-                return 5;
-            case ConsoleKey.J:
-                return 6;
-            case ConsoleKey.K:
-                return 7;
-            case ConsoleKey.L:
-                return 8;
-            case ConsoleKey.Z:
-                return 9;
-            case ConsoleKey.X:
-                return 10;
-            case ConsoleKey.C:
-                return 11;
-            default:
-                return -1;
-        }
+        case 1:
+            return secondOctave;
+        case 2:
+            return threeOctave;
+        default:
+            return secondOctave;
     }
+}
+
+
 
     
 
